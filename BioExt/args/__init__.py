aa5998620bc4540e8268e0dffdc16e75e53ec82c
Dave Bouvier <dave@bx.psu.edu>
2021-06-28 09:30:57 -0400

    Add SARS-CoV-2 references and fix biopython 1.78 compatibility

--- BioExt/args/__init__.py
+++ BioExt/args/__init__.py
@@ -27,7 +27,7 @@ def add_alphabet(parser, *args):
 def add_reference(parser, *args):
     from argparse import ArgumentTypeError
     from Bio import SeqIO
-    from BioExt.references import hxb2, nl4_3
+    from BioExt.references import hxb2, nl4_3, cov2
 
     references = {
         'HXB2_env': hxb2.env,
@@ -43,7 +43,35 @@ def add_reference(parser, *args):
         'HXB2_vif': hxb2.vif,
         'HXB2_vpr': hxb2.vpr,
         'HXB2_vpu': hxb2.vpu,
-        'NL4-3_prrt': nl4_3.prrt
+        'NL4-3_prrt': nl4_3.prrt,
+        'CoV2-3C': cov2.threeC,
+        'CoV2-E': cov2.E,
+        'CoV2-endornase': cov2.endornase,
+        'CoV2-exonuclease': cov2.exonuclease,
+        'CoV2-helicase': cov2.helicase,
+        'CoV2-leader': cov2.leader,
+        'CoV2-methyltransferase': cov2.methyltransferase,
+        'CoV2-M': cov2.M,
+        'CoV2-N': cov2.N,
+        'CoV2-nsp10': cov2.nsp10,
+        'CoV2-nsp2': cov2.nsp2,
+        'CoV2-nsp3': cov2.nsp3,
+        'CoV2-nsp4': cov2.nsp4,
+        'CoV2-nsp6': cov2.nsp6,
+        'CoV2-nsp7': cov2.nsp7,
+        'CoV2-nsp8': cov2.nsp8,
+        'CoV2-nsp9': cov2.nsp9,
+        'CoV2-ORF10': cov2.ORF10,
+        'CoV2-ORF1a': cov2.ORF1a,
+        'CoV2-ORF1b': cov2.ORF1b,
+        'CoV2-ORF3a': cov2.ORF3a,
+        'CoV2-ORF5': cov2.ORF5,
+        'CoV2-ORF6': cov2.ORF6,
+        'CoV2-ORF7a': cov2.ORF7a,
+        'CoV2-ORF7b': cov2.ORF7b,
+        'CoV2-ORF8': cov2.ORF8,
+        'CoV2-RdRp': cov2.RdRp,
+        'CoV2-S': cov2.S
         }
 
     def reference(string):
--- BioExt/references/__init__.py
+++ BioExt/references/__init__.py
@@ -6,10 +6,11 @@ from os.path import join
 from BioExt.references._factory import _refdir, _reffactory
 
 
-__all__ = ['hxb2', 'nl4_3']
+__all__ = ['hxb2', 'nl4_3', 'cov2']
 
 
 # annoyingly, glob doesn't support curly bracket {fa,gb}-style wildcards
 # so use this bracket-hack
 hxb2 = _reffactory(join(_refdir, 'hxb2'), 'HXB2_%s.[fg][ab]')
 nl4_3 = _reffactory(join(_refdir, 'nl4-3'), 'NL4_3_%s.fna')
+cov2 = _reffactory(join(_refdir, 'cov2'), 'CoV2-%s.fa')
